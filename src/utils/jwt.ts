1a00d8e9656a5fee6736d26417e1b91a8188bb4c
jqllxew <a787841563@gmail.com>
2025-05-26 16:47:05 +0800

    fix: custom jwt

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,40 +1,24 @@
-import { SignJWT, importJWK } from 'jose';
 import { sha256 } from 'js-sha256';
-import { JWT_SECRET_KEY } from '@/const/auth';
+import base64url from 'base64url';
+import { JWT_SECRET_KEY } from "@/const/auth";
 
-export const createJWT = async <T>(payload: T) => {
+function encode(obj: object) {
+  return base64url.encode(JSON.stringify(obj));
+}
+
+export const createJWT = async (payload: any = {}) => {
   const now = Math.floor(Date.now() / 1000);
-  const duration = 3600; // 3600s
-
-  const encoder = new TextEncoder();
-
-  let secretKey;
-  // fix the issue that crypto.subtle is not available in non-HTTPS environment
-  // refs: https://github.com/lobehub/lobe-chat/pull/1238
-  if (crypto.subtle) {
-    // create a secret key
-    secretKey = await crypto.subtle.digest('SHA-256', encoder.encode(JWT_SECRET_KEY));
-  } else {
-    // const buffer = encoder.encode(JSON.stringify(payload));
-    // return `${NON_HTTP_PREFIX}.${Buffer.from(buffer).toString('base64')}`;
-    secretKey = sha256.arrayBuffer(encoder.encode(JWT_SECRET_KEY));
-  }
-
-
-
-  // get the JWK from the secret key
-  const jwkSecretKey = await importJWK(
-    {
-      k: Buffer.from(secretKey).toString('base64'),
-      kty: 'oct',
-    },
-    'HS256',
-  );
-
+  const header = { alg: 'HS256', typ: 'JWT' };
   // 创建JWT
-  return new SignJWT(payload as Record<string, any>)
-    .setProtectedHeader({ alg: 'HS256' })
-    .setIssuedAt(now) // 设置JWT的iat（签发时间）声明
-    .setExpirationTime(now + duration) // 设置 JWT 的 exp（过期时间）为 100 s
-    .sign(jwkSecretKey);
+  payload.iat = now;
+  payload.exp = now + 3600;
+
+  const headerEnc = encode(header);
+  const payloadEnc = encode(payload);
+  const data = `${headerEnc}.${payloadEnc}`;
+  const hashedKey = sha256.array(JWT_SECRET_KEY);
+  const sigHex = sha256.hmac(hashedKey, data); // => hex string
+  const sigB64 = base64url(Buffer.from(sigHex, 'hex'));
+
+  return `${data}.${sigB64}`;
 };
